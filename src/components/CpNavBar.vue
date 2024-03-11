a5bc734a71effb06137c3adb37cb894aaddddf1c
ycjtd <17336404952@163.com>
2024-03-11 14:01:49 +0800

    feta:cp-nav-bar的结构和样式

---
+++ src/components/CpNavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="cp-nav-bar">
+    <van-nav-bar
+      fixed
+      left-arrow
+      title="登录"
+      right-text="注册"
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+    />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+:deep() {
+  .van-nav-bar {
+    &__arrow {
+      color: var(--cp-text1);
+      font-size: 18px;
+    }
+    &__title {
+      font-size: 15px;
+    }
+  }
+}
+</style>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="home">
-    <Button>d</Button>
-    <van-radio-group v-model="checked">
-      <van-radio name="1">单选框 1</van-radio>
-      <van-radio name="2">单选框 2</van-radio>
-    </van-radio-group>
+    <cp-nav-bar></cp-nav-bar>
   </div>
 </template>
 
