1d582490fe2cb3e2ca3ebfe97e1935206dde793e
ycjtd <17336404952@163.com>
2024-03-11 14:32:06 +0800

    feta:标题和右侧文字以及点击右侧文字自定义事件

--- src/components/CpNavBar.vue
+++ src/components/CpNavBar.vue
@@ -3,15 +3,29 @@
     <van-nav-bar
       fixed
       left-arrow
-      title="登录"
-      right-text="注册"
-      @click-left="onClickLeft"
+      :title="title"
+      :right-text="rightText"
       @click-right="onClickRight"
     />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {defineProps} from 'vue'
+// 1.通过props实现标题和右侧文字的设置
+defineProps<{
+  title?: string
+  rightText?: string
+}>()  // definePropsAPI是一个函数，它的返回值就是父亲传来的属性。
+
+// 2.使用emit函数来触发自定义事件（点击右侧文字按钮）
+const emit = defineEmits<{
+  (e:'click-right'):void
+}>()
+const onClickRight = () => {
+  emit('click-right')
+}
+</script>
 
 <style scoped lang="scss">
 :deep() {
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="home">
-    <cp-nav-bar></cp-nav-bar>
+  <div class="login">
+    <cp-nav-bar @click-right="fn" title="登录" right-text="注册"></cp-nav-bar>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const fn = () => {
+  console.log('触发了');
+}
+</script>
 
 <style scoped lang="scss">
-.home {
-  color: red;
+.login {
 }
 </style>
